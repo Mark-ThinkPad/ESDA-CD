3b8d8635f1a16aa3c3376fb7b4f869d5dd97dcd8
Mark-ThinkPad <1372469698@qq.com>
2020-06-05 18:48:05 +0800

    course design

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-from PyQt5 import QtSql
-from PyQt5.QtSql import QSqlDatabase
+import sys
+from PyQt5 import QtWidgets
 
 if __name__ == '__main__':
-    print(QSqlDatabase)
+    app = QtWidgets.QApplication(sys.argv)
+    hello = QtWidgets.QLabel('Hello, World!')
+    hello.show()
+    sys.exit(app.exec_())
